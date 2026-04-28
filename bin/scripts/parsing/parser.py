f4715c505520cbbbe337024463f6cfe94c226e09
Madlirex <122043382+Madlirex@users.noreply.github.com>
2026-04-28 22:16:16 +0200

    feat: start parser

---
+++ bin/scripts/parsing/parser.py
@@ -0,0 +1,6 @@
+from scripts.misc import constants
+from scripts.misc.constants import KEYWORDS_LIST, SWAPPED_KEYWORDS, BRACKET_PAIRS
+from scripts.tokenizing import token
+from scripts.tokenizing.token import TokenType, Token
+from scripts.misc import node
+from scripts.misc.node import *
